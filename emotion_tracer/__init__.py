45769c749cfec7be3d717bb463bcf41c7953adee
Hyonyoung Choi <hyonchoi@seas.upenn.edu>
2026-05-28 21:30:11 -0400

    fix: restore full __init__.py with all 6 class exports

--- emotion_tracer/__init__.py
+++ emotion_tracer/__init__.py
@@ -1,12 +1,15 @@
 from emotion_tracer.config import EmotionTracerConfig
+from emotion_tracer.model_wrapper import ModelWrapper
+from emotion_tracer.story_generator import StoryGenerator
+from emotion_tracer.emotion_vectors import EmotionVectorBuilder
+from emotion_tracer.trajectory import TrajectoryExtractor
+from emotion_tracer.visualizer import Visualizer
 
 __all__ = [
     "EmotionTracerConfig",
+    "ModelWrapper",
+    "StoryGenerator",
+    "EmotionVectorBuilder",
+    "TrajectoryExtractor",
+    "Visualizer",
 ]
-
-# Forward imports for modules created later (uncomment as they are implemented):
-# from emotion_tracer.model_wrapper import ModelWrapper
-# from emotion_tracer.story_generator import StoryGenerator
-# from emotion_tracer.emotion_vectors import EmotionVectorBuilder
-# from emotion_tracer.trajectory import TrajectoryExtractor
-# from emotion_tracer.visualizer import Visualizer
